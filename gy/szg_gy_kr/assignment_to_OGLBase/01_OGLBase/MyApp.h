2d0d59d13fece6fcde73ffb10310a15081f5a276
Emese <8emi95@inf.elte.hu>
2019-12-18 19:18:31 +0100

    keringő, forgó, y-on fel/le mozgó Z-k

---
+++ gy/szg_gy_kr/assignment_to_OGLBase/01_OGLBase/MyApp.h
@@ -0,0 +1,68 @@
+#pragma once
+
+// C++ includes
+#include <memory>
+#include <vector>
+
+// GLEW
+#include <GL/glew.h>
+
+// SDL
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform2.hpp>
+
+#include "gCamera.h"
+
+#include "ProgramObject.h"
+#include "BufferObject.h"
+#include "VertexArrayObject.h"
+#include "TextureObject.h"
+
+class CMyApp
+{
+public:
+	CMyApp(void);
+	~CMyApp(void);
+
+	bool Init();
+	void Clean();
+
+	void Update();
+	void Render();
+
+	void KeyboardDown(SDL_KeyboardEvent&);
+	void KeyboardUp(SDL_KeyboardEvent&);
+	void MouseMove(SDL_MouseMotionEvent&);
+	void MouseDown(SDL_MouseButtonEvent&);
+	void MouseUp(SDL_MouseButtonEvent&);
+	void MouseWheel(SDL_MouseWheelEvent&);
+	void Resize(int, int);
+protected:
+	// segédeljárások
+	void TextureFromFileAttach(const char* filename, GLuint role) const;
+	void DrawTetris(std::vector<glm::vec3>, std::vector<glm::vec3>);
+	void DrawCube(glm::mat4);
+
+	// shaderekhez szükséges változók
+	ProgramObject		m_program;			// shaderek programja
+	ProgramObject		m_programSkybox;	// skybox shaderek
+
+	VertexArrayObject	m_vao;				// VAO objektum
+	IndexBuffer			m_gpuBufferIndices;	// indexek
+	ArrayBuffer			m_gpuBufferPos;		// pozíciók tömbje
+
+	gCamera				m_camera;
+
+	// nyers OGL azonosítók
+	GLuint				m_skyboxTexture;
+
+	bool speedUp;
+	bool slowDown;
+	bool stagnate;
+};
+
