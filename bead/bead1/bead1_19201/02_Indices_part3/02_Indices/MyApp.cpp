919a0f257205a3e9c26e62fbe482cd99541bd2f9
Emese <8emi95@inf.elte.hu>
2020-05-17 19:56:38 +0200

    19201 bead part 3 (revised)

--- bead/bead1/bead1_19201/02_Indices_part3/02_Indices/MyApp.cpp
+++ bead/bead1/bead1_19201/02_Indices_part3/02_Indices/MyApp.cpp
@@ -64,8 +64,8 @@ glm::vec3 CMyApp::get_random_point()
 	std::cout << "\n";
 	*/
 
-	int x = get_random_int(0, possible_points.size()-1);
-	int z = get_random_int(0, possible_points.size()-1);
+	int x = get_random_int(0, possible_points.size() - 1);
+	int z = get_random_int(0, possible_points.size() - 1);
 	float y = (x / 2) ^ 3 - x * z / 4 + (z / 2) ^ 2;
 	return glm::vec3(possible_points[x], y, possible_points[z]);
 }
@@ -85,7 +85,7 @@ bool CMyApp::Init()
 	//
 
 	Vertex vert[] =
-	{ 
+	{
 		//          x,  y, z             R, G, B
 		{glm::vec3(0,0,0), glm::vec3(0,0,0)},		//0
 		{glm::vec3(0,1,0), glm::vec3(0,1,0)},		//1
@@ -98,11 +98,11 @@ bool CMyApp::Init()
 	};
 
 	// indexpuffer adatai
-    GLushort indices[]=
-    {
+	GLushort indices[] =
+	{
 		//FRONT
-        0,3,2,
-        3,0,1,
+		0,3,2,
+		3,0,1,
 		//RIGHT
 		4,2,5,
 		2,3,5,
@@ -118,7 +118,7 @@ bool CMyApp::Init()
 		//BOTTOM
 		4,6,0,
 		4,0,2
-    };
+	};
 
 	for (int i = 0; i < 9; i++) {
 		glm::vec3 generated_point = get_random_point();
@@ -136,18 +136,18 @@ bool CMyApp::Init()
 	glGenVertexArrays(1, &m_vaoID);
 	// a frissen generált VAO beallitasa aktívnak
 	glBindVertexArray(m_vaoID);
-	
+
 	// hozzunk létre egy új VBO erõforrás nevet
-	glGenBuffers(1, &m_vboID); 
+	glGenBuffers(1, &m_vboID);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vboID); // tegyük "aktívvá" a létrehozott VBO-t
 	// töltsük fel adatokkal az aktív VBO-t
-	glBufferData( GL_ARRAY_BUFFER,	// az aktív VBO-ba töltsünk adatokat
-				  sizeof(vert),		// ennyi bájt nagyságban
-				  vert,	// errõl a rendszermemóriabeli címrõl olvasva
-				  GL_STATIC_DRAW);	// úgy, hogy a VBO-nkba nem tervezünk ezután írni és minden kirajzoláskor felhasnzáljuk a benne lévõ adatokat
-	
+	glBufferData(GL_ARRAY_BUFFER,	// az aktív VBO-ba töltsünk adatokat
+		sizeof(vert),		// ennyi bájt nagyságban
+		vert,	// errõl a rendszermemóriabeli címrõl olvasva
+		GL_STATIC_DRAW);	// úgy, hogy a VBO-nkba nem tervezünk ezután írni és minden kirajzoláskor felhasnzáljuk a benne lévõ adatokat
 
-	// VAO-ban jegyezzük fel, hogy a VBO-ban az elsõ 3 float sizeof(Vertex)-enként lesz az elsõ attribútum (pozíció)
+
+// VAO-ban jegyezzük fel, hogy a VBO-ban az elsõ 3 float sizeof(Vertex)-enként lesz az elsõ attribútum (pozíció)
 	glEnableVertexAttribArray(0); // ez lesz majd a pozíció
 	glVertexAttribPointer(
 		0,				// a VB-ben található adatok közül a 0. "indexû" attribútumait állítjuk be
@@ -156,17 +156,17 @@ bool CMyApp::Init()
 		GL_FALSE,		// normalizalt legyen-e
 		sizeof(Vertex),	// stride (0=egymas utan)
 		0				// a 0. indexû attribútum hol kezdõdik a sizeof(Vertex)-nyi területen belül
-	); 
+	);
 
 	// a második attribútumhoz pedig a VBO-ban sizeof(Vertex) ugrás után sizeof(glm::vec3)-nyit menve újabb 3 float adatot találunk (szín)
 	glEnableVertexAttribArray(1); // ez lesz majd a szín
 	glVertexAttribPointer(
 		1,
-		3, 
+		3,
 		GL_FLOAT,
 		GL_FALSE,
 		sizeof(Vertex),
-		(void*)(sizeof(glm::vec3)) );
+		(void*)(sizeof(glm::vec3)));
 
 	// index puffer létrehozása
 	glGenBuffers(1, &m_ibID);
@@ -180,8 +180,8 @@ bool CMyApp::Init()
 	//
 	// shaderek betöltése
 	//
-	GLuint vs_ID = loadShader(GL_VERTEX_SHADER,		"myVert.vert");
-	GLuint fs_ID = loadShader(GL_FRAGMENT_SHADER,	"myFrag.frag");
+	GLuint vs_ID = loadShader(GL_VERTEX_SHADER, "myVert.vert");
+	GLuint fs_ID = loadShader(GL_FRAGMENT_SHADER, "myFrag.frag");
 
 	// a shadereket tároló program létrehozása
 	m_programID = glCreateProgram();
@@ -192,10 +192,10 @@ bool CMyApp::Init()
 
 	// VAO-beli attribútumok hozzárendelése a shader változókhoz
 	// FONTOS: linkelés elõtt kell ezt megtenni!
-	glBindAttribLocation(	m_programID,	// shader azonosítója, amibõl egy változóhoz szeretnénk hozzárendelést csinálni
-							0,				// a VAO-beli azonosító index
-							"vs_in_pos");	// a shader-beli változónév
-	glBindAttribLocation( m_programID, 1, "vs_in_col");
+	glBindAttribLocation(m_programID,	// shader azonosítója, amibõl egy változóhoz szeretnénk hozzárendelést csinálni
+		0,				// a VAO-beli azonosító index
+		"vs_in_pos");	// a shader-beli változónév
+	glBindAttribLocation(m_programID, 1, "vs_in_col");
 
 	// illesszük össze a shadereket (kimenõ-bemenõ változók összerendelése stb.)
 	glLinkProgram(m_programID);
@@ -205,14 +205,14 @@ bool CMyApp::Init()
 
 	glGetProgramiv(m_programID, GL_LINK_STATUS, &result);
 	glGetProgramiv(m_programID, GL_INFO_LOG_LENGTH, &infoLogLength);
-	if ( GL_FALSE == result )
+	if (GL_FALSE == result)
 	{
-		std::vector<char> ProgramErrorMessage( infoLogLength );
+		std::vector<char> ProgramErrorMessage(infoLogLength);
 		glGetProgramInfoLog(m_programID, infoLogLength, NULL, &ProgramErrorMessage[0]);
 		fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
-		
+
 		char* aSzoveg = new char[ProgramErrorMessage.size()];
-		memcpy( aSzoveg, &ProgramErrorMessage[0], ProgramErrorMessage.size());
+		memcpy(aSzoveg, &ProgramErrorMessage[0], ProgramErrorMessage.size());
 
 		std::cout << "[app.Init()] Sáder Huba panasza: " << aSzoveg << std::endl;
 
@@ -220,18 +220,18 @@ bool CMyApp::Init()
 	}
 
 	// mar nincs ezekre szukseg
-	glDeleteShader( vs_ID );
-	glDeleteShader( fs_ID );
+	glDeleteShader(vs_ID);
+	glDeleteShader(fs_ID);
 
 	//
 	// egyéb inicializálás
 	//
 
 	// vetítési mátrix létrehozása
-	m_matProj = glm::perspective( 45.0f, 640/480.0f, 1.0f, 1000.0f );
+	m_matProj = glm::perspective(45.0f, 640 / 480.0f, 1.0f, 1000.0f);
 
 	// shader-beli transzformációs mátrixok címének lekérdezése
-	m_loc_mvp = glGetUniformLocation( m_programID, "MVP");
+	m_loc_mvp = glGetUniformLocation(m_programID, "MVP");
 
 	return true;
 }
@@ -242,7 +242,7 @@ void CMyApp::Clean()
 	glDeleteBuffers(1, &m_ibID);
 	glDeleteVertexArrays(1, &m_vaoID);
 
-	glDeleteProgram( m_programID );
+	glDeleteProgram(m_programID);
 }
 
 void CMyApp::Update()
@@ -250,9 +250,9 @@ void CMyApp::Update()
 	// nézeti transzformáció beállítása
 	float t = SDL_GetTicks() / 1000.0f;
 	//m_matView = glm::lookAt(glm::vec3(5 * cosf(t), 5, 5 * sinf(t)),	// honnan nézzük a színteret
-	m_matView = glm::lookAt(glm::vec3( 20,  20,  20),		// honnan nézzük a színteret
-							glm::vec3( 0,  0,  0),		// a színtér melyik pontját nézzük
-							glm::vec3( 0,  1,  0));		// felfelé mutató irány a világban
+	m_matView = glm::lookAt(glm::vec3(20, 20, 20),		// honnan nézzük a színteret
+		glm::vec3(0, 0, 0),		// a színtér melyik pontját nézzük
+		glm::vec3(0, 1, 0));		// felfelé mutató irány a világban
 }
 
 
@@ -262,7 +262,7 @@ void CMyApp::Render()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// shader bekapcsolasa
-	glUseProgram( m_programID );
+	glUseProgram(m_programID);
 
 	// shader parameterek beállítása
 	/*
@@ -295,11 +295,11 @@ void CMyApp::Render()
 		rotate_var = glm::vec3(1, 0, 0);
 	}
 
-	for(int i = 0; i < 9; i++)
+	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 5; j++) {
 			m_matWorld =
-				glm::translate<float>(generated_points[i])*
+				glm::translate<float>(generated_points[i]) *
 				glm::translate<float>(cube_shift[j]) *
 				glm::rotate<float>(rotate_angle, rotate_var);
 
@@ -325,7 +325,7 @@ void CMyApp::Render()
 	glBindVertexArray(0);
 
 	// shader kikapcsolasa
-	glUseProgram( 0 );
+	glUseProgram(0);
 }
 
 void CMyApp::KeyboardDown(SDL_KeyboardEvent& key)
@@ -361,8 +361,8 @@ void CMyApp::Resize(int _w, int _h)
 {
 	glViewport(0, 0, _w, _h);
 
-	m_matProj = glm::perspective(  45.0f,		// 90 fokos nyilasszog
-									_w/(float)_h,	// ablakmereteknek megfelelo nezeti arany
-									0.01f,			// kozeli vagosik
-									100.0f);		// tavoli vagosik
+	m_matProj = glm::perspective(45.0f,		// 90 fokos nyilasszog
+		_w / (float)_h,	// ablakmereteknek megfelelo nezeti arany
+		0.01f,			// kozeli vagosik
+		100.0f);		// tavoli vagosik
 }
